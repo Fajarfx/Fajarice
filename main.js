56f5f0d63244813d7cbbd2f185d6941d0dc2970d
Fajarfx <112601577+Fajarfx@users.noreply.github.com>
2024-08-31 22:11:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+let clicked = false;
 
+document.getElementById('startButton').addEventListener('mouseover', function() {
+    if (!clicked) {
+        this.classList.add('moved');
+    }
+});
+
+document.getElementById('startButton').addEventListener('mouseout', function() {
+    if (!clicked) {
+        this.classList.remove('moved');
+    }
+});
+
+document.getElementById('startButton').addEventListener('click', function() {
+    if (!clicked) {
+        clicked = true;
+        this.innerText = "Eh, Serius? 😜";
+        this.classList.add('tricked');
+        setTimeout(() => {
+            alert("Hahaha, ketipu! 😆 Sekarang baru benar-benar mulai belajar yuk, Mufa! 🚀");
+            this.innerText = "Oke, Sekarang Mulai Serius!";
+            this.classList.remove('tricked');
+            clicked = false;
+        }, 2000);
+    }
+});
